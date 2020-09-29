2b4d9ebcd6906a2dbb1879867f3a2c8e3fa7d47e
Srinivas nalla <nallasrinivas678@outlook.com>
2020-09-28 21:38:52 -0400

    Install entityframework core ,entity framework in memory & automapper with dependency injection
    Create Db context for products
    Create Product class

---
+++ eCommerce.Api.Products/Db/ProductDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Api.Products.Db
+{
+    public class ProductDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+    }
+}
