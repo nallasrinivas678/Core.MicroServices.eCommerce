75113a4665980fc86702fcdcc6f4fcea1abc3f0a
Srinivas nalla <nallasrinivas678@outlook.com>
2020-10-05 20:15:27 -0400

    Added search model

---
+++ eCommerce.Api.Search/Models/SearchTerm.cs
@@ -0,0 +1,11 @@
+using eCommerce.Api.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Api.Search.Models
+    public class SearchTerm
+{
+    public int CustomerId { get; set; }
+}
